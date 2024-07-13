7e08024cd95bd2194195c17de063d20a30125894
gorilskij <43991400@users.noreply.github.com>
2024-07-13 10:41:13 +0200

    basic implementation of metavault

--- src/MetaVault.sol
+++ src/MetaVault.sol
@@ -35,6 +35,7 @@ contract MetaVault is Ownable, ERC4626 {
                 if (!vaults[i].enabled) {
                     vaults[i].enabled = true;
                     numEnabled++;
+                    ERC20(CRVUSD).approve(_vault, type(uint256).max);
                 }
                 return;
             }
@@ -50,8 +51,76 @@ contract MetaVault is Ownable, ERC4626 {
                 if (vaults[i].enabled) {
                     vaults[i].enabled = false;
                     numEnabled--;
+                    ERC20(CRVUSD).approve(_vault, 0);
                 }
                 return;
+            }
+        }
+
+        revert("not found");
+    }
+
+    function _deposit(
+        address caller,
+        address receiver,
+        uint256 assets,
+        uint256 shares
+    ) internal override {
+        super._deposit(caller, receiver, assets, shares);
+        _allocateDeposit(assets);
+    }
+
+    function _allocateDeposit(uint256 amount) internal {
+        require(amount > 0);
+
+        // find the vault with the most space
+        uint256 total = 0;
+        uint256[] memory spaces = new uint256[](vaults.length);
+        for (uint256 i = 0; i < vaults.length; i++) {
+            if (vaults[i].enabled) {
+                total += vaults[i].amount;
+                spaces[i] = vaults[i].target + EPSILON;
+            }
+        }
+
+        uint256 maxI = 0;
+        uint256 maxV = 0;
+        for (uint256 i = 0; i < vaults.length; i++) {
+            if (vaults[i].enabled) {
+                uint256 a = spaces[i] * total;
+                uint256 b = vaults[i].amount;
+                if (a <= b) {
+                    spaces[i] = 0;
+                } else {
+                    // 10_000 for fraction precision
+                    spaces[i] = ((a - b) * amount * 10_000) / total;
+
+                    if (spaces[i] > maxV) {
+                        maxI = i;
+                        maxV = spaces[i];
+                    }
+                }
+            }
+        }
+
+        ERC20(CRVUSD).transferFrom(msg.sender, address(this), amount);
+        if (amount <= maxV) {
+            // deposit amount into vault maxI
+            IVault(vaults[maxI].addr).deposit(amount);
+        } else {
+            // deposit maxV into vault maxI
+            for (uint256 i = 0; i < vaults.length; i++) {
+                if (i != maxI && vaults[i].enabled) {
+                    if (amount <= spaces[i]) {
+                        // deposit amount into vault i
+                        IVault(vaults[i].addr).deposit(amount);
+                        break;
+                    } else {
+                        // deposit spaces[i] into vault i
+                        IVault(vaults[maxI].addr).deposit(spaces[i]);
+                        amount -= spaces[i];
+                    }
+                }
             }
         }
     }
